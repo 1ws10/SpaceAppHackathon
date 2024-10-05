ae93829234e15381a30aca7cbf8833e9c8837d3a
cam <cameronangled@gmail.com>
2024-10-05 09:04:28 -0400

    removing user pages for initial building

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
-import Login from './Login';
-import Register from './Register';
+//import Login from './Login';
+//import Register from './Register';
 import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,16 +14,17 @@ const App = () => {
       <Navbar bg="light" expand="lg">
         <Navbar.Brand as={Link} to="/">MyApp</Navbar.Brand>
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/login">Login</Nav.Link>
-          <Nav.Link as={Link} to="/register">Register</Nav.Link>
+          {/*<Nav.Link as={Link} to="/login">Login</Nav.Link>
+          <Nav.Link as={Link} to="/register">Register</Nav.Link>*/}
           <Nav.Link as={Link} to="/home">Home</Nav.Link>
         </Nav>
       </Navbar>
 
       <Routes>
-        <Route path="/login" element={<Login setToken={setToken} />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={token ? <Home /> : <Navigate to="/login" />} />
+        {/*<Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/register" element={<Register />} />*/}
+        {/*<Route path="/home" element={token ? <Home /> : <Navigate to="/login" />} /> */}
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
