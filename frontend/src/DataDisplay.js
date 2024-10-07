ebd43e3eee6fc0a4762df514427fb50e243f50bf
DonYall <highvoltage9886@gmail.com>
2024-10-06 22:53:49 -0400

    support for multiple data points

--- frontend/src/DataDisplay.js
+++ frontend/src/DataDisplay.js
@@ -9,6 +9,7 @@ import wavelengths from "./constants";
 
 const DataDisplay = () => {
   const location = useLocation();
+  const [pixelIndex, setPixelIndex] = useState(0);
   const [pixelData, setPixelData] = useState(null);
   const [graphData, setGraphData] = useState(null);
   const [error, setError] = useState(null);
@@ -55,16 +56,30 @@ const DataDisplay = () => {
     fetchPixelData();
   }, []);
 
-  const handleDataReceived = (data) => {
-    setPixelData(data);
-    console.log("Received data:", data); // Log the received data
-    const graphData = Object.keys(data.selectedPixel)
+  useEffect(() => {
+    if (!pixelData) return;
+    const graphData = Object.keys(pixelData.pixelValues[pixelIndex]) // Get keys from the selected pixel data
       .filter((key) => wavelengths[key]) // Filter out keys without corresponding wavelengths
       .map((key) => {
         return {
           band: key,
           wavelength: wavelengths[key],
-          reflectance: data.selectedPixel[key] / 100000,
+          reflectance: pixelData.pixelValues[pixelIndex][key] / 100000, // Normalize reflectance values
+        };
+      });
+    setGraphData(graphData);
+  }, [pixelIndex]);
+
+  const handleDataReceived = (data) => {
+    setPixelData(data);
+    console.log("Received data:", data); // Log the received data
+    const graphData = Object.keys(data.pixelValues[pixelIndex]) // Get keys from the selected pixel data
+      .filter((key) => wavelengths[key]) // Filter out keys without corresponding wavelengths
+      .map((key) => {
+        return {
+          band: key,
+          wavelength: wavelengths[key],
+          reflectance: data.pixelValues[pixelIndex][key] / 100000, // Normalize reflectance values
         };
       });
     setGraphData(graphData);
@@ -110,8 +125,11 @@ const DataDisplay = () => {
   }
 
   return (
-    <div>
+    <div className="w-full">
       <h1>Landsat 9 Data for Selected Pixel</h1>
+      {/* gif */}
+      <img src={pixelData.pixelValues[pixelIndex].thumbnail} alt="Landsat 9 Image" />
+
       {/* <div>
         <h3>Selected Pixel Data:</h3>
         <pre>{JSON.stringify(pixelData.selectedPixel, null, 2)}</pre>
@@ -121,25 +139,48 @@ const DataDisplay = () => {
         <pre>{JSON.stringify(pixelData.surroundingPixels, null, 2)}</pre>
       </div> */}
       <div className="chart-container">
-        <h3>Wavelength Reflectance Chart</h3>
+        <h3>Wavelength Reflectance Chart for {pixelData.pixelValues[pixelIndex].date}</h3>
         <WavelengthChart graphData={graphData} />
       </div>
-      {/* download json */}
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(pixelData));
-          const downloadAnchorNode = document.createElement("a");
-          downloadAnchorNode.setAttribute("href", dataStr);
-          downloadAnchorNode.setAttribute("download", "landsat_data.json");
-          document.body.appendChild(downloadAnchorNode); // required for firefox
-          downloadAnchorNode.click();
-          downloadAnchorNode.remove();
-        }}
-      >
-        Download JSON
-      </Button>
+      <section className="w-full flex justify-between my-4">
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={pixelIndex === 0}
+          onClick={() => {
+            if (pixelIndex === 0) return;
+            setPixelIndex((prev) => prev - 1);
+          }}
+        >
+          Previous Date
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(pixelData));
+            const downloadAnchorNode = document.createElement("a");
+            downloadAnchorNode.setAttribute("href", dataStr);
+            downloadAnchorNode.setAttribute("download", "landsat_data.json");
+            document.body.appendChild(downloadAnchorNode); // required for firefox
+            downloadAnchorNode.click();
+            downloadAnchorNode.remove();
+          }}
+        >
+          Download JSON
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={pixelIndex === pixelData.pixelValues.length - 1}
+          onClick={() => {
+            if (pixelIndex === pixelData.pixelValues.length - 1) return;
+            setPixelIndex((prev) => prev + 1);
+          }}
+        >
+          Next Date
+        </Button>
+      </section>
       {/* MUI Email Popup Modal */}
       <Dialog open={showPopup} onClose={handleClosePopup}>
         <DialogTitle>Enter email for notifications on the next LandSat overpass</DialogTitle>
